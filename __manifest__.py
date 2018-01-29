2b760d37da762b8a43b5688a0fb9fd2a8790cb67
Phuc Le Dien <phucledien@gmail.com>
2018-01-29 08:42:18 +0700

    First commit

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name':'Tutorial theme',
+    'description':'A description for your theme',
+    'version':'1.0',
+    'author':'phucledien',
+
+    'data': [
+        'views/layout.xml',
+        'views/pages.xml',
+        'views/assets.xml'   
+    ],
+    'category':'Theme/Creative',
+    'depends': ['website', 'website_sale', 'sale'],
+}
